860181145be30c6e1da25da4cf09868b7c13d068
Ihauenyi Donald <ihuaenyi@gmail.com>
2023-07-08 13:56:20 +0100

    implemented is_even function

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def is_even(number):
+    if number  % 2 == 0:
+        print('number {0} is even'.format(number))
+    else:
+        print('number {0} is odd'.format(number))
+        
+        
+        
+if __name__ == '__main__':
+    
+
+
+    is_even(11)
